06c2c1266a40d6cc2f57ee16f900078a38cd866b
lth-1026 <th20001026@gmail.com>
2025-06-06 01:15:02 +0900

    [feat] ExtendPrompt에 canExtendLoan, extenableLaon, extendLoan 구현

--- src/main/java/org/example/com/prompt/user/ExtendPrompt.java
+++ src/main/java/org/example/com/prompt/user/ExtendPrompt.java
@@ -1,41 +1,79 @@
 package org.example.com.prompt.user;
 
+import org.example.com.model.Loan;
 import org.example.com.model.User;
+import org.example.com.util.DateManager;
 import org.example.com.util.LoanManager;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class ExtendPrompt {
     private final Scanner scanner = new Scanner(System.in);
     private final User currentUser;
-    private final LoanManager loanManager = new LoanManager();
 
     public ExtendPrompt(User user) {
         this.currentUser = user;
     }
 
     public void start() {
+        LoanManager.getUserLoanRecord(currentUser.getId());
         while(true) {
             if(currentUser.getTotalPenalty() > 0) {
                 System.out.println("!!패널티 상태입니다. 도서를 연장할 수 없습니다.");
                 return;
             }
 
-            System.out.println("연장할 책의 ISBN을 입력해주세요::");
+            System.out.print("연장할 책의 ISBN을 입력해주세요::");
             String isbn = scanner.nextLine();
 
-            //TODO: 입력한 ISBN이 사용자가 대출 중인 도서에 존재하는지 확인
-            if (true) {
+            if (!LoanManager.isIsbnExists(isbn)) {
                 System.out.println("!! 현재 대출 중인 도서가 아닙니다.");
+                return; //돌아가기
             }
 
-            //TODO: 이미 연장된 도서를 다시 연장하려고 할 경우
-            if(true) {
+            if(!canExtendLoan(isbn)) {
                 System.out.println("!! 해당 도서는 이미 연장되었습니다.");
+                return;
             }
 
-            //TODO: 올바른 ISBN이 입력되고 연장 가능할 경우, 해당 도서의 반납예정일에 +7일 연장되고 연장 여부가 'Y'로 변경됩니다.
-            System.out.println();
+            extendLoan(isbn);
+            return;
         }
+    }
+
+    private boolean canExtendLoan(String isbn) {
+        return extendableLoan(isbn).isPresent();
+    }
+
+    private Optional<Loan> extendableLoan(String isbn) {
+        return LoanManager.getUserNowLoanRecord().stream()
+                .filter(loan -> loan.getIsbn().equals(isbn))
+                .sorted(Comparator.comparing(loan -> LocalDate.parse(loan.getLoanDate())))
+                .filter(loan -> !loan.isExtended())
+                .findFirst();
+    }
+
+    private void extendLoan(String isbn) {
+        Optional<Loan> optionalLoan = extendableLoan(isbn);
+        if(optionalLoan.isEmpty()) {
+            System.out.println("처리 중 오류가 생겼습니다.");
+            return;
+        }
+        Loan loan = optionalLoan.get();
+        loan.setExtended(true);
+        LocalDate dueDate = LocalDate.parse(loan.getDueDate());
+        LocalDate newDueDate = dueDate.plusDays(7);
+        loan.setDueDate(newDueDate.toString());
+        LoanManager.updateLoan();
+        LocalDate today = DateManager.loadDateFromFile();
+//         오늘(today)부터 newReturnDate까지 남은 일수 계산
+        assert today != null;
+        long daysLeft = ChronoUnit.DAYS.between(today, newDueDate);
+        System.out.println(loan.getTitle() + "도서의 반납 예정일이 " + newDueDate.getYear() + "년 " + newDueDate.getMonthValue() + "월 " + newDueDate.getDayOfMonth()+"로 연장되었습니다.");
+        System.out.println("현재 "+ currentUser.getLoanCount() + "권 대출 중이며, 연장된 반납일까지 " + daysLeft + "일 남았습니다.");
     }
 }
